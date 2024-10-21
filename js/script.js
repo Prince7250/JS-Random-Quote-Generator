0b3652c2378c1151925cd594bbd28af5e9a0a32e
Prince7250 <agrawalprince115@gmail.com>
2024-10-21 17:25:24 +0530

    Delete comment line

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-//Object Array to hold quotes, sources, citaitons and years
 var quotes = [
 	{
 	quote: "Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.",
@@ -163,14 +161,12 @@ var quotes = [
 	}
 ];
 
-//Function to randomly select a quote value and return a random quote object from the quotes array
 function getRandomQuote () {
 	var randomNumber = Math.floor(Math.random() * (quotes.length));
 	var randomQuote = quotes[randomNumber];
 	return randomQuote;
 }	
 
-//Function to select random rgb color value
 function getRandomColor () {
 	var red = Math.floor(Math.random() * 256 );
 	var green = Math.floor(Math.random() * 256 );
@@ -179,8 +175,6 @@ function getRandomColor () {
 	return randomColor;
 }
 
-//Function to call the getRandomQuote function and stores the returned quote object in a variable
-//Constructs a string containing the different properties of the quote object 
 function printQuote () {
 		var quotes = getRandomQuote ();
 		var quoteContainer = document.getElementById("quote-box");
@@ -190,16 +184,13 @@ function printQuote () {
 				else {quoteString += '</p>'};
 					quoteContainer.innerHTML = quoteString;
 
-					//assigns random color value to document background color 
 					document.body.style.backgroundColor = getRandomColor ();
 }
 
-//Quote automatically refreshes every 15 seconds
 window.setInterval(function(){
 printQuote ();
 }, 15000);
-
-//Event listener on LoadQuote button to generate new quote		
+		
 document.getElementById("loadQuote").addEventListener("click", printQuote, false);
 
 
